8af61fa3da92cfcec447c40db2165b0dae158eac
Pablo Bustos <pablobustos609@gmail.com>
2022-12-01 03:39:47 -0300

    started register view

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,27 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import TestSocket from '../views/TestSocket.vue'
+import Register from "../views/Register.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    // {
+    //   path: '/',
+    //   name: 'Chat',
+    //   component: HomeView
+    // },
+
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    },
-    {
-      path: "/testSocket",
-      name: "testSocket",
-      component: TestSocket
+      path: "/register",
+      name: "Register",
+      component: Register
     }
   ]
 })
---
+++ client/src/views/Register.vue
@@ -0,0 +1,63 @@
+<script>
+    export default{
+        name: "Register"
+    }
+</script>
+
+<template>
+    <div class="container">
+        <h1 class="title">Register</h1>
+        <form class="form">
+            <input class="form--input" type="text" placeholder="name"/>
+            <input class="form--input" type="text" placeholder="username"/>
+            <input class="form--input" type="text" placeholder="email"/>
+            <input class="form--input" type="text" placeholder="password"/>
+
+            <button class="form--button">Create account</button>
+        </form>
+    </div>
+</template>
+
+<style scoped>
+    .container{
+        width: 100%;
+        height: 100%;
+        
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        
+    }
+
+    .form{
+        width: 500px;
+        height: 500px;
+        /* background-color: #101010; */
+        /* background-color: #2B2B2B; */
+
+        display: flex;
+        flex-direction: column;
+        justify-content: space-evenly;
+    }
+
+    .form--input{
+        height: 60px;
+        padding-left: 10px;
+        
+        background-color: transparent;
+        border: 1px solid #7a7a7a;
+        border-radius: 10px;
+        box-sizing: border-box;
+
+        font-size: 25px;
+        color: #7a7a7a;
+    }
+
+    .form--button{
+        height: 40px;
+        border-radius: 10px;
+        font-size: 20px;
+        cursor: pointer;
+    }
+</style>
